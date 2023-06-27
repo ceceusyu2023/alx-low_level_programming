a025638cde2b7ad01fb863475ce7c9d76112d0e7
ceceusyu2023 <cecendululuusyu@gmail.com>
2023-06-27 02:18:31 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive \n", n);
+		printf("%dispositive \n", n);
 	else if (n == 0)
-		printf("%d is zero \n", n);
+		printf("%diszero \n", n);
 	else
-		printf("%d is negative \n", n);
+		printf("%disnegative \n", n);
 	return (0);
 }
