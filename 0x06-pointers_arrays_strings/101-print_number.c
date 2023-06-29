65b95bc93ff16dc9194f99e3c3ef5f5caf8ab392
ceceusyu2023 <cecendululuusyu@gmail.com>
2023-06-29 16:11:27 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#include" main.h"
+#include "main.h"
 
 /**
  * print_number - print numbers chars
@@ -23,5 +23,4 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
-	return (0);
 }
