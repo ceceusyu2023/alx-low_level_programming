ab47c730d641e1fb337971379074293dd1f7277d
ceceusyu2023 <cecendululuusyu@gmail.com>
2023-06-29 15:05:10 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int m;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (m = 0; m < 52; m++)
+		{
+			if (s[i] == data1[m])
+			{
+				s[i] = datarot[m];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
