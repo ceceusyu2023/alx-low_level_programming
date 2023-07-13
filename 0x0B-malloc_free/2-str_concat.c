eb30364d88abd5ef48dc99db33c89911bba1942e
ceceusyu2023 <cecendululuusyu@gmail.com>
2023-07-13 06:02:04 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,12 +30,12 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] != '\0')
 	{
 		concat[i] = s1[i];
-		i++
+		i++;
 	}
 	while (s2[ci] != '\0')
 	{
 		concat[i] = '\0';
 		return (concat);
 	}
+	return (concat)
 }
-
